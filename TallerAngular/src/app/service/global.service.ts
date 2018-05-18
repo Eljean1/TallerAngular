0a0234df35b8983a945015e6839bbea6064d5f09
Kevin Araya Reygada <kevxar@gmail.com>
2018-05-18 00:38:11 -0400

    Actualización de API

--- TallerAngular/src/app/service/global.service.ts
+++ TallerAngular/src/app/service/global.service.ts
@@ -5,7 +5,7 @@ import { OnInit } from '@angular/core';
 export class GlobalService implements OnInit {
   constructor() { }
   //private url = 'https://newsapi.org/v2/top-headlines?sources=national-geographic&apiKey=ed6bb68169694928beada76957626d34&callback=?';
-  private url = 'https://sheetsu.com/apis/v1.0su/409608f78be2';
+  private url = 'https://sheetsu.com/apis/v1.0su/c5fbb990d30b';
   ngOnInit() {
     console.log(this.url);
   }
